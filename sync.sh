0097ec83fc5aef5a91309179e37284bb797afcdb
chrscato <christophercato7@gmail.com>
2025-05-05 20:07:23 -0400

    Auto-deploy: Mon, May  5, 2025  8:07:23 PM

--- sync.sh
+++ sync.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-# Pull latest changes from git repository
-ssh root@159.223.104.254 "cd /opt/network_dev && git pull" 
+# Pull latest changes from git repository and sync to both directories
+ssh root@159.223.104.254 "cd /opt/network_dev && git pull && \
+    mkdir -p /srv/network_dev && \
+    cp -r /opt/network_dev/* /srv/network_dev/" 
