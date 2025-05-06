6ecb506f1b9e66e56c8046249afeeac5ebe18238
chrscato <christophercato7@gmail.com>
2025-05-05 20:10:59 -0400

    Auto-deploy: Mon, May  5, 2025  8:10:59 PM

--- pull.sh
+++ pull.sh
@@ -6,5 +6,6 @@ ssh root@159.223.104.254 "cd /opt/network_dev && \
         rm -rf * && \
         git clone https://github.com/chrscato/network_dev.git .; \
     else \
-        git pull origin master; \
+        git fetch origin && \
+        git reset --hard origin/master; \
     fi" 
